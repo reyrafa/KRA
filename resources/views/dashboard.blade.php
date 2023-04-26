ec810f1259502387f888e146a340828c054889a9
Rey Costemiano <rafaelcostemiano05@gmail.com>
2023-04-26 14:29:53 +0800

    rafael commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5">
           
-                Hi There  hi im changing here
+                Hi There  hi im changing here hello
                 @foreach($systemUser as $systemInfo)
                     @foreach($branch as $branchInfo)
                         @if($systemInfo->branchUnderID == $branchInfo->id)
